3da225101af122907e9bcfb4887d2e950f673102
Pamela Fett <pamelafett@gmail.com>
2022-09-27 13:45:16 +0100

    first commit

--- src/br/com/dio/model/Gato.java
+++ src/br/com/dio/model/Gato.java
@@ -1,15 +1,12 @@
 package br.com.dio.model;
 
-import java.util.Objects;
-
 public class Gato {
 
 	private String nome;
 	private String cor;
 	private Integer idade;
 	
-	public Gato() {
-		}
-	}
+	
+}
 
 
