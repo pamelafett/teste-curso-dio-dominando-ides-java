37893a3a7ab6752eacc7bb34d856f780c4791439
Pamela Fett <pamelafett@gmail.com>
2022-09-27 13:34:07 +0100

    first commit

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,16 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		Gato gato = new Gato();
+		Livro livro = new Livro();
+		
+		System.out.println(gato);
+		System.out.println(livro);
+	}
+
+}
+
---
+++ src/br/com/dio/model/Gato.java
@@ -0,0 +1,15 @@
+package br.com.dio.model;
+
+import java.util.Objects;
+
+public class Gato {
+
+	private String nome;
+	private String cor;
+	private Integer idade;
+	
+	public Gato() {
+		}
+	}
+
+
